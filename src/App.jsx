656377da3657482ba6cb96694837dcfc274b7ceb
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-07-16 09:09:58 -0300

    Commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components"
+
+const fundoGradiente = styled.div`
+
+    background: linear-gradient();
+
+`
+
+function App() {
+
+  return (
+    <fundoGradiente>
+      <h1> Olá mundo!</h1>
+    </fundoGradiente>
+  )
+}
+
+export default App
